85bbb735801e192bdf20d98f62a0f0c07fa25997
Ethan Brooks <ethanabrooks@gmail.com>
2020-02-06 12:23:36 -0500

    almost have latex working but not actually compiling

--- src/index.js
+++ src/index.js
@@ -1,125 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { Helmet } from "react-helmet";
+import { parse, HtmlGenerator } from "latex.js";
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
+const divStyle = {
+  color: "blue",
+  backgroundImage: "url( http://www.hvallison.com/assets/1915-008a.jpg )"
+};
+const dom = (
+  <html lang="en">
+    <Helmet>
+      <meta charset="UTF-8" />
+      <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+      <meta http-equiv="content-language" content="en" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <script type="module">
+        import latexjs from
+        "https://cdn.jsdelivr.net/npm/latex.js/dist/latex.component.esm.js"
+        customElements.define('latex-js', latexjs)
+      </script>
+      <title>LaTeX.js Web Component Test</title>
+    </Helmet>
+    <h1>Compiling LaTeX</h1>
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
+    <latex-js baseURL="https://cdn.jsdelivr.net/npm/latex.js@0.11.1/dist/">
+      {`
+\\documentclass{article}
+\\begin{document}
+Hello World.
+\\end{document}
+    `}
+    </latex-js>
 
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
+    <latex-js hyphenate="false">Another $x$.</latex-js>
+  </html>
+);
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null)
-        }
-      ],
-      xIsNext: true
-    };
-  }
-
-  handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      history: history.concat([
-        {
-          squares: squares
-        }
-      ]),
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[history.length - 1];
-    const winner = calculateWinner(current.squares);
-
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
-
-ReactDOM.render(<Game />, document.getElementById("root"));
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+ReactDOM.render(dom, document.getElementById("root"));
